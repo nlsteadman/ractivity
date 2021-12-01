8993c8b38ad01eda70e8981491881b547eb00ade
Nicole Steadman <nsteadman@gmail.com>
2021-12-01 09:55:23 -0800

    making button work

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById("activityButton");
     const container = document.getElementById("activityDisplay");
+    
 
     button.addEventListener('click', () => {
-        fetch('http://www.boredapi.com/api/activity/')
-        .then(response => response.json())
-        .then(data => {
-            renderEvent(data)
-        })
+        fetch("http://www.boredapi.com/api/activity/")
+            .then(response => response.json())
+            .then(data => {
+                renderActivity(data)
+            })
     })
-    function renderEvent(data) {
-        container.append(data)
-    }
 
+    function renderActivity(activity) {
+        const p = document.createElement("p");
+        p.textContent = activity
+        container.textContent = ''
+        container.appendChild(p)
+    
+    }
 
 })
