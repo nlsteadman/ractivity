e9d3ea040fc69c30b54a549507c877377455d410
Nicole Steadman <nsteadman@gmail.com>
2021-12-02 09:46:23 -0800

    more troubleshooting

--- script.js
+++ script.js
@@ -2,25 +2,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById("activityButton");
     const container = document.getElementById("activityDisplay");
     
-    function renderActivity(randomActivity) {
-        container.innerHTML = ''
-        const p = document.createElement("p");
-        p.classname = "activity"
-        p.innerHTML = randomActivity.activity;
-        
-        container.appendChild(p);
+    
 
-        container.fadeIn(500);
-    }
     function getActivity() {
         button.addEventListener('click', () => {
-            fetch("http://www.boredapi.com/api/activity/")
+            fetch("https://www.boredapi.com/api/activity/")
                 .then(response => response.json())
                 .then(data => {
                     renderActivity(data)
                 })
+                
         })
     }
 
     getActivity();
+
+
+    function renderActivity(randomActivity) {
+        button.addEventListener('click', () => {
+            
+            container.innerHTML = ''
+            const p = document.createElement("p");
+            p.classname = "activity"
+            p.innerHTML = randomActivity.activity;
+            
+            container.appendChild(p);
+
+            container.fadeIn(500);
+        })
+    }
 })
+
