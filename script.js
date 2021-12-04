ac2a78b7a6c3b307e51f0dc0d559ed88e87ff255
Nicole Steadman <nsteadman@gmail.com>
2021-12-03 17:55:07 -0800

    still troubleshooting like button

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const button = document.getElementById("activityButton");
     const container = document.getElementById("activityDisplay");
     const like = document.getElementById("likeButton");
-    const likeContainer = document.getElementById("likedActivities")
+    const likeContainer = document.getElementById("likedActivities");
     
 
     function getActivity() {
@@ -12,36 +12,31 @@ document.addEventListener('DOMContentLoaded', () => {
                 .then(data => {
                     renderActivity(data)
                 })
-        })
+            })
     }
 
     getActivity();
 
 
     function renderActivity(randomActivity) {
-        button.addEventListener('click', () => {
-            
-            container.innerHTML = ''
-            const p = document.createElement("p");
-            p.classname = "activity"
-            p.innerHTML = randomActivity.activity;
-            
-            container.appendChild(p);
-
-        })
+        
+        container.innerHTML = ''
+        const p = document.createElement("p");
+        p.classname = "activity"
+        p.innerHTML = randomActivity.activity;
+        
+        container.appendChild(p);
+        keepActivity(p.innerHTML);
     }
 
-    function keepActivity(p) {
+    function keepActivity(currentActivity) {
         like.addEventListener('click', () => {
-
             const li = document.createElement("li");
             li.classname = "liked"
-            li.innerHTML = p;
-
-            likeContainer.appendChild(li);
-
+            li.innerHTML = currentActivity;
+            
+            likeContainer.append(li);
         })
     }
-    keepActivity();
 })
 
