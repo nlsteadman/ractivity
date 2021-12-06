2cec11252ca8d67ba7a06b398bef1a561b2a73c7
Nicole Steadman <nsteadman@gmail.com>
2021-12-06 08:28:36 -0800

    moved DOMContentLoaded to make code easier to read

--- script.js
+++ script.js
@@ -1,40 +1,42 @@
+const button = document.getElementById("activityButton");
+const container = document.getElementById("activityDisplay");
+const like = document.getElementById("likeButton");
+const likeContainer = document.getElementById("likedActivities");
+
 document.addEventListener('DOMContentLoaded', () => {
-    const button = document.getElementById("activityButton");
-    const container = document.getElementById("activityDisplay");
-    const like = document.getElementById("likeButton");
-    const likeContainer = document.getElementById("likedActivities");
-
-    function getActivity() {
-        button.addEventListener('click', () => {
-            fetch("https://www.boredapi.com/api/activity/")
-                .then(response => response.json())
-                .then(data => {
-                    renderActivity(data)
-                })
-        })
-    }
-
-    getActivity();
-
-
-    function renderActivity(randomActivity) {
-        
-        container.innerHTML = ''
-        const p = document.createElement("p");
-        p.innerHTML = randomActivity.activity;
-        
-        container.appendChild(p);
-        keepActivity(p.innerHTML);
-    }
-
-    function keepActivity(currentActivity) {
-        debugger;
-        const li = document.createElement("li");
-        li.innerHTML = currentActivity;
-
-        like.addEventListener('click', () => {
-            likeContainer.appendChild(li);
-        })
-    }
+    getActivity()
 })
+
+function getActivity() {
+    button.addEventListener('click', () => {
+        fetch("https://www.boredapi.com/api/activity/")
+            .then(response => response.json())
+            .then(data => {
+                renderActivity(data)
+            })
+    })
+}
+
+getActivity();
+
+
+function renderActivity(randomActivity) {
+    
+    container.innerHTML = ''
+    const p = document.createElement("p");
+    p.innerHTML = randomActivity.activity;
+    
+    container.appendChild(p);
+    keepActivity(p.innerHTML);
+}
+
+function keepActivity(currentActivity) {
+    debugger;
+    const li = document.createElement("li");
+    li.innerHTML = currentActivity;
+
+    like.addEventListener('click', () => {
+        likeContainer.append(li);
+    })
+}
 
